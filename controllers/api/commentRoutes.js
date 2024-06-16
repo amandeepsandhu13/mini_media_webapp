41b652fcd7ea4dff71d7a54c21cd1000563fb50c
jaquelineesteves <jaaqfariasz@gmail.com>
2024-06-16 10:04:34 -0400

    see coments and add comments in handlebar

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,25 +2,20 @@ const router = require("express").Router();
 const { Comment,Post } = require("../../models/index");
 const { withAuth, withAuthApi } = require("../../utils/auth");
 
-// to see all the comments
 
-
-router.post('/', async (req, res) => {
-    try { 
-        const newComment = await 
-      Comment.create({
-        ...req.body,
-        userId:req.session.userId,
-      })
-      
+router.post("/", async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            
+        });
       res.status(200).json(newComment);
-      res.render('add-comment');
       }
       catch(err){
         console.log(err);
         res.status(400).json(err);
       }
-    // create a new comment
+
   });
 
 router.delete('/:id', async (req, res) => {
--- public/js/add-comment.js
+++ public/js/add-comment.js
@@ -1,29 +1,34 @@
 document.querySelector('#new-comment-form').addEventListener('submit', async function newCommentHandler(event) {
-    event.preventDefault();
-    
-    const comment_content = document.querySelector('#comentcontent').value;
-    const userId = window.userId;
-    const postId = event.target.hasAttribute('data-id');
+  event.preventDefault();
+
+  const comment_content = document.querySelector('#comment_content').value;
 
 
-    const response = await fetch(`/api/comments/${postId}/${commentId}`, {
+  if ( !comment_content) {
+      alert('comment content is missing');
+      return;
+  }
+
+  const response = await fetch(`/api/comments`, { 
       method: 'POST',
       body: JSON.stringify({
-        comment_content,
-        userId,
-        postId,
+          comment_content,
+    
       }),
       headers: {
-        'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
       },
-    });
-    
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
+  });
+
+  if (response.ok) {
+      document.location.replace('/comments');
+  } else {
       alert('Failed to add comment');
-    }
+  }
 });
+
+
+
 
 // // to delete a comment
 // const deleteCommentHandler = async (event) => {
