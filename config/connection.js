d464a0734f0aae679c552820ab6d546da36a17cd
Amandeep Sandhu <38268269+amandeepsandhu13@users.noreply.github.com>
2024-06-17 14:00:00 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,26 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
-    }
-  );
+      protocol: 'postgres',
+      logging: false, // Disable logging; default: console.log
+      dialectOptions: {
+        ssl: {
+          require: true, // This is necessary for SSL connection to Render PostgreSQL
+          rejectUnauthorized: false, // This may be necessary based on your PostgreSQL setup on Render
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+      }
+    );
 
 module.exports = sequelize;
